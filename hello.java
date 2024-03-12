25849c9aa0f074a51cc5f68f69c3cab0ca32f5d3
Farhan-Labib-afk <farhan.labib@ucalgary.ca>
2024-03-11 22:25:17 -0600

    Labib sexy

--- hello.java
+++ hello.java
@@ -2,5 +2,6 @@ public class hello{
     public static void main(String[] args){
         system.out.println("Hello World")
 //Sexy lady
-    }
+        // Labib Ahmed's wife will be Sumaiya
+        }
 }
